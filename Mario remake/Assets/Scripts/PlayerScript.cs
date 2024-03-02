4f79b6f6da7f525d6bf6f9eadea8b591fa4680ad
DieFlugel233 <112236368+DieFlugel233@users.noreply.github.com>
2024-03-02 15:10:44 +1100

    Update PlayerScript.cs

--- Mario remake/Assets/Scripts/PlayerScript.cs
+++ Mario remake/Assets/Scripts/PlayerScript.cs
@@ -1,76 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float jumpForce = 100.0f;
+    public float jumpForce = 15.0f;
     private Rigidbody2D rb;
-    public bool isGrounded;
-    private Vector2 movement;
-    private GameObject player;
-    private SpriteRenderer spriteRenderer;
-    private Transform groundedCheck;
-    private ContactFilter2D filter;
-    private Collider2D[] colliders = new Collider2D[1];
+    private bool isGrounded;
 
     void Start()
     {
-        rb = this.gameObject.GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        groundedCheck = GameObject.FindWithTag("GroundedCheck").transform;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    void Update()
     {
-        Flip();
-        IsGrounded();
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        movement = new Vector2(moveHorizontal, 0.0f);
-        rb.MovePosition(new Vector2(transform.position.x, transform.position.y) + movement * speed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
-        {
-            Debug.Log("jump");
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
-        }
+        rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
     }
 
-    /*void OnCollisionEnter(Collision other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
-        }
-    }*/
-
-    private void IsGrounded()
-    {
-        if (Physics2D.OverlapBox(groundedCheck.position, groundedCheck.localScale, 0, filter, colliders) > 0)
-        {
-            isGrounded = true;
+            isGrounded = true; 
+            Debug.Log("Grounded");
         }
     }
-
-
-
-    public void Flip()
-        {
-            if (movement.x < 0)
-            {
-                spriteRenderer.flipX = false;
-            }
-            else if (movement.x > 0)
-            {
-                spriteRenderer.flipX = true;
-            }
-        }
-    }
-
-
+}
