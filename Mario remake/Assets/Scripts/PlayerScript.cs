638f956f2288c2c1bafbd9cf1f2534147d157aa7
speedyseal69 <111723174+speedyseal69@users.noreply.github.com>
2024-03-03 16:45:39 +1100

    1

--- Mario remake/Assets/Scripts/PlayerScript.cs
+++ Mario remake/Assets/Scripts/PlayerScript.cs
@@ -8,9 +8,17 @@ public class PlayerScript : MonoBehaviour
     public float jumpForce = 15.0f;
     public Vector3 growthFactor = new Vector3(1.5f, 1.5f, 1);
     private Rigidbody2D rb;
-    private bool isGrounded;
     private bool isBig = false;
     private Vector3 originalSize; 
+    public GameObject UiManager;
+    public bool isGrounded;
+
+    public GameObject underworldSpawn;
+
+    public GameObject overworldSpawn;
+
+    public bool onPipe = false;
+    public bool nextToPipe = false;
 
     void Start()
     {
@@ -29,6 +37,21 @@ public class PlayerScript : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             isGrounded = false;
         }
+         if (Input.GetKeyDown("s"))
+        {
+            if (onPipe)
+            {
+                this.transform.position = underworldSpawn.transform.position;
+                //Debug.Log("Pipe");
+            }
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            if (nextToPipe)
+            {
+                this.transform.position = overworldSpawn.transform.position;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -37,6 +60,7 @@ public class PlayerScript : MonoBehaviour
         rb.velocity = new Vector2(moveHorizontal * speed, rb.velocity.y);
     }
 
+   
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Ground"))
@@ -46,6 +70,25 @@ public class PlayerScript : MonoBehaviour
         else if (other.gameObject.CompareTag("Goomba") && isBig)
         {
             Destroy(other.gameObject); 
+        }
+        if (other.gameObject.CompareTag("Box")) {
+            UIManager uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+            uiManager.boxToCoin(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("UWPipe"))
+        {
+            onPipe = true;
+        }
+        else if (other.gameObject.CompareTag("OWPipe")) {
+             nextToPipe = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("UWPipe") || collision.gameObject.CompareTag("OWPipe"))
+        {
+            onPipe = false;
+            nextToPipe = false;
         }
     }
 
